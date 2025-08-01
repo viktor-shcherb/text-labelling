2982f2f1d2b9910c348dfcb5ae6b0cb435a82227
Viktor Shcherbakov <viktoroo.sch@gmail.com>
2025-08-01 14:18:00 +0200

    Switch icon file

--- src/label_app/ui/main.py
+++ src/label_app/ui/main.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import streamlit as st
 
-ICON_PATH = Path(__file__).with_name("static") / "assignment.svg"
+ICON_PATH = Path(__file__).with_name("static") / "icon.svg"
 st.set_page_config(page_title="Text Label App", page_icon=str(ICON_PATH), layout="wide")
 
 st.write("## Welcome to Text Label App")
